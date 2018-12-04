dbe720fce9f7a060356e64fba093625916219cd4
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-05 00:07:54 +0900

    Add Get, Post request at main page, modify index.html with formModel

--- src/main/java/com/jaytech/springbootjpa/controllers/MainController.java
+++ src/main/java/com/jaytech/springbootjpa/controllers/MainController.java
@@ -4,19 +4,31 @@ import com.jaytech.springbootjpa.domain.MyData;
 import com.jaytech.springbootjpa.repositories.MyDataRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
 
 @Controller
 public class MainController {
     @Autowired
     private MyDataRepository myDataRepository;
 
-    @RequestMapping("/")
-    public String index(Model model) {
+    @RequestMapping(value = "/", method = RequestMethod.GET)
+    public String index(@ModelAttribute("formModel") MyData myData, Model model) {
         Iterable<MyData> list = myDataRepository.findAll();
-        model.addAttribute("data",list);
+        model.addAttribute("datalist",list);
+
         return "index";
     }
 
+    @RequestMapping(value = "/", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public String form(@ModelAttribute("formModel") MyData myData) {
+        myDataRepository.saveAndFlush(myData);
+
+        return "redirect:/";
+    }
 }
