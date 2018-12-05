0189ce232242777e3fe2ab8c9c148af523571bc4
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-06 00:13:29 +0900

    Add test JPQL in repository

--- src/main/java/com/jaytech/springbootjpa/repositories/MyDataRepository.java
+++ src/main/java/com/jaytech/springbootjpa/repositories/MyDataRepository.java
@@ -4,9 +4,15 @@ import com.jaytech.springbootjpa.domain.MyData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface MyDataRepository extends JpaRepository<MyData, Long> {
     Optional<MyData> findById(Long name);
+
+    List<MyData> findByNameLike(String name);
+    List<MyData> findByIdIsNotNullOrderByIdDesc();
+    List<MyData> findByAgeGreaterThan(Integer age);
+    List<MyData> findByAgeBetween(Integer age1, Integer age2);
 }
