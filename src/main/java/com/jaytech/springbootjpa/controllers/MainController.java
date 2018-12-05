a8d19195f9c8fe3f93bf120f5dd79698bdf38504
pjh374968@gmail.com <pjh374968@gmail.com>
2018-12-05 22:12:35 +0900

    Create Edit data page, controller, repository

--- src/main/java/com/jaytech/springbootjpa/controllers/MainController.java
+++ src/main/java/com/jaytech/springbootjpa/controllers/MainController.java
@@ -7,14 +7,27 @@ import org.springframework.stereotype.Controller;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.annotation.PostConstruct;
 
 
 @Controller
 public class MainController {
     @Autowired
     private MyDataRepository myDataRepository;
+
+    @PostConstruct
+    public void init() {
+        MyData d1 = new MyData();
+        d1.setName("kim");
+        d1.setAge(123);
+        d1.setMail("fdfdf@df.com");
+        d1.setMemo("This is a memo");
+        myDataRepository.saveAndFlush(d1);
+    }
 
     @RequestMapping(value = "/", method = RequestMethod.GET)
     public String index(@ModelAttribute("formModel") MyData myData, Model model) {
@@ -27,6 +40,25 @@ public class MainController {
     @RequestMapping(value = "/", method = RequestMethod.POST)
     @Transactional(readOnly = false)
     public String form(@ModelAttribute("formModel") MyData myData) {
+        myDataRepository.saveAndFlush(myData);
+
+        return "redirect:/";
+    }
+
+    @RequestMapping(value = "/edit/{id}", method = RequestMethod.GET)
+    public String edit(@PathVariable int id, Model model) {
+        MyData myData = myDataRepository.findById((long)id).orElse(null);
+
+        if(myData != null) {
+            model.addAttribute("formModel", myData);
+        }
+
+        return "edit";
+    }
+
+    @RequestMapping(value = "/edit", method = RequestMethod.POST)
+    @Transactional(readOnly = false)
+    public String update(@ModelAttribute("formModel") MyData myData) {
         myDataRepository.saveAndFlush(myData);
 
         return "redirect:/";
--- src/main/java/com/jaytech/springbootjpa/repositories/MyDataRepository.java
+++ src/main/java/com/jaytech/springbootjpa/repositories/MyDataRepository.java
@@ -4,6 +4,9 @@ import com.jaytech.springbootjpa.domain.MyData;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface MyDataRepository extends JpaRepository<MyData, Long> {
+    Optional<MyData> findById(Long name);
 }
